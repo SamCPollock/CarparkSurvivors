0665bba663b3b0b56f19e16926f24805a061eb9c
mikeytpollock <miketpollock+github@gmail.com>
2024-05-17 11:47:04 -0700

    Added ram
    sorta works - there is some bad things with pylons I think / some ground elements.

---
+++ Assets/_Scripts/Ram_Mono.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ram_Mono : MonoBehaviour
+{
+    public int damage = 1;
+    public float forceMagnitude = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        Enemy_Mono collidedEnemy = other.GetComponent<Enemy_Mono>();
+        if (collidedEnemy != null)
+        {
+            other.GetComponent<Enemy_Mono>().TakeDamage(damage);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Get the Rigidbody component of the object we collided with
+        Rigidbody rb = collision.collider.GetComponent<Rigidbody>();
+
+        // If the collided object has a Rigidbody, apply the force
+        if (rb != null)
+        {
+            // Calculate direction away from the impact point
+            Vector3 forceDirection = collision.transform.position - transform.position;
+            forceDirection.Normalize();
+
+            // Apply the force
+            rb.AddForce(forceDirection * forceMagnitude, ForceMode.Impulse);
+        }
+    }
+
+
+    /*
+    ///When hitting an enemy
+    ///Do damage
+    private void OnCollisionEnter(Collision other)
+    {
+        Enemy_Mono collidedEnemy = other.GetComponent<Enemy_Mono>();
+        if (collidedEnemy != null)
+        {
+            other.GetComponent<Enemy_Mono>().TakeDamage(damage);
+        }
+    }
+    */
+}
